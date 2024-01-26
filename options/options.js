7cf323e8c9f45b1231fdecfe1d0190f06a617228
Crowdbotics <team@crowdbotics.com>
2024-01-26 18:47:50 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hut-hut-data-feed-a-45580",
+  url: "https://hut-hut-data-feed-a-45580.botics.co",
+  api: "https://hut-hut-data-feed-a-45580.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
